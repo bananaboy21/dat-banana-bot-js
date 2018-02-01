ab73a851e218e6491cb05210918110fa6c30184a
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-02-01 17:43:26 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+const client = new Discord.Client();
+
+client.on("ready" => {
+ console.log(`Logged in as ${client.user.name}`);
+});
+
+client.on("message", message => {
+if(message.content.startsWith(config.prefix + "ping")) {
+ message.reply("Pong!");
+}
+});
+
+client.login(config.token);
